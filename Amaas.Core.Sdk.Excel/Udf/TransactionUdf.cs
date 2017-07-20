0022832586d3e3ae3da9e6acd8e585a75049e0aa
JYL123 <li474412210@gmail.com>
2017-07-20 18:26:30 +0800

    Add function description and Argument description. Add start date, end date, page size ,page no. params for each function.

--- Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
+++ Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
@@ -6,43 +6,49 @@ namespace Amaas.Core.Sdk.Excel.Udf
 {
     public class TransactionUdf
     {
-        [ExcelFunction(Name = "GetPosition")]
-        public static object GetPositionUdf([ExcelArgument(AllowReference = true)] string AMID, string bookID) => ArrayResizer.Resize(new TransactionUdf().GetPositions(AMID, bookID), "Position");
+        [ExcelFunction(Name = "GetPosition", Description = "Get positions")]
+        [ExcelCommand(ShortCut = "GP")]
+        public static object GetPositionUdf([ExcelArgument(AllowReference = true)] string AMID, string bookID, string startDate, string pageSize, string pageNum) =>
+           new TransactionUdf().GetPositions(AMID, bookID, startDate, pageSize, pageNum);
 
-        [ExcelFunction(Name = "GetTransactionByBookID")]
+        [ExcelFunction(Name = "GetTransactionByBookID", Description = "Get transactions according to Book ID")]
+        [ExcelCommand(ShortCut = "GTB")]
         public static object GetTransactionByBookIDUdf(
-            [ExcelArgument(AllowReference = true)] string AMID, string bookID) => 
-            new TransactionUdf().GetTransactionByBookID(AMID, bookID);
+            [ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(Name = "bookID")]string bookID, [ExcelArgument(Name = "startDate")]string startDate, [ExcelArgument(Name = "endDate")]string endDate, [ExcelArgument(Name = "pageSize")]string pageSize, [ExcelArgument(Name = "pageNum")]string pageNum) =>
+            new TransactionUdf().GetTransactionByBookID(AMID, bookID, startDate, endDate, pageSize, pageNum);
 
-        [ExcelFunction(Name = "GetTransactionByTransactionID")]
+        [ExcelFunction(Name = "GetTransactionByTransactionID", Description = "Get transactions according to transaction ID")]
+        [ExcelCommand(ShortCut = "GTT")]
         public static object GetTransactionByTransactionIDUdf(
-            [ExcelArgument(AllowReference = true)] string AMID, string transactionID) =>
-            new TransactionUdf().GetTransactionByTransactionID(AMID, transactionID);
+            [ExcelArgument(AllowReference = true)] string AMID, string transactionID, string startDate, string endDate, string pageSize, string pageNum) =>
+            new TransactionUdf().GetTransactionByTransactionID(AMID, transactionID, startDate, endDate, pageSize, pageNum);
 
-        public object[,] GetPositions(string AMID, string bookID)
+        public object GetPositions(string AMID, string bookID, string startDate, string pageSize, string pageNum)
         {
-            object[,] getArray;
-            if (bookID == ""||AMID == "") return getArray= new object[1, 1] { {"Resource ID is required"}};//error message
+            //if (bookID == "") return "Book ID is required";
+            //if (AMID == "") return "Asset Manager ID is required";
             TransactionDataAccess TDA = new TransactionDataAccess();
-            getArray = TDA.Position(AMID, bookID);
-            return getArray;//for unit tests, we don't use-->return  ArrayResizer.Resize(getArray, "position");
+            object[,] getArray = TDA.Position(AMID, bookID, startDate, pageSize, pageNum);
+            return ArrayResizer.Resize(getArray, "position");
         }
 
-        public object GetTransactionByTransactionID(string AMID, string transactionID)
+        public object GetTransactionByTransactionID(string AMID, string transactionID, string startDate, string endDate, string pageSize, string pageNum)
         {
-            if (transactionID == "") return "Transaction ID is required";
-            if (AMID == "") return "Asset Manager ID is required";
+            //if (transactionID == "") return "Transaction ID is required";
+            //if (AMID == "") return "Asset Manager ID is required";
+            // Worksheet.Range["A1", "B1"].Interior.Color = Excel.XlRgbColor.rgbDarkBlue;
+
             TransactionDataAccess TDA = new TransactionDataAccess();
-            object[,] getArray = TDA.Transaction(AMID, transactionID, "TransactionByTransactionID");
+            object[,] getArray = TDA.Transaction(AMID, transactionID, startDate, endDate, pageSize, pageNum, "TransactionByTransactionID");
             return ArrayResizer.Resize(getArray, "transactions");
         }
 
-        public object GetTransactionByBookID(string AMID, string bookID)
+        public object GetTransactionByBookID(string AMID, string bookID, string startDate, string endDate, string pageSize, string pageNum)
         {
-            if (bookID == "") return "Book ID is required";
-            if (AMID == "") return "Asset Manager ID is required";
+            //if (bookID == "") return "Book ID is required";
+            //if (AMID == "") return "Asset Manager ID is required";
             TransactionDataAccess TDA = new TransactionDataAccess();
-            object[,] getArray = TDA.Transaction(AMID, bookID, "TransactionByBookID");
+            object[,] getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, "TransactionByBookID");
             return ArrayResizer.Resize(getArray, "transactions");
         }
     }
