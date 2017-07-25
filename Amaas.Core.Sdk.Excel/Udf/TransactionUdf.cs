09d6ae6957c52e7ebd1cc61cd621e438d1ec878f
JYL123 <li474412210@gmail.com>
2017-07-25 14:12:50 +0800

    Wrap UDFs as asynchrous functions

--- Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
+++ Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
@@ -1,27 +1,81 @@
-﻿using ExcelDna.Integration;
+using ExcelDna.Integration;
 using Amaas.Core.Sdk.Excel.DataAccess;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace Amaas.Core.Sdk.Excel.Udf
 {
     public class TransactionUdf
     {
-        [ExcelFunction(Name = "GetPosition", Description = "Get positions")]
-        [ExcelCommand(ShortCut = "GP")]
-        public static object GetPositionUdf([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(Name = "book ID")]string bookID, [ExcelArgument(Name = "startDate")]string startDate, [ExcelArgument(Name = "pageSize")]string pageSize, [ExcelArgument(Name = "pageNum")]string pageNum) =>
-           new TransactionUdf().GetPositions(AMID, bookID, startDate, pageSize, pageNum);
+        public void AutoOpen()
+        {
+            ExcelIntegration.RegisterUnhandledExceptionHandler(
+                ex => "!!! EXCEPTION: " + ex.ToString());
+        }
+        public void AutoClose()
+        {
+        }     
 
-        [ExcelFunction(Name = "GetTransactionByBookID", Description = "Get transactions according to Book ID")]
-        [ExcelCommand(ShortCut = "GTB")]
-        public static object GetTransactionByBookIDUdf(
-            [ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(Name = "book ID")]string bookID, [ExcelArgument(Name = "startDate")]string startDate, [ExcelArgument(Name = "endDate")]string endDate, [ExcelArgument(Name = "pageSize")]string pageSize, [ExcelArgument(Name = "pageNum")]string pageNum) =>
-            new TransactionUdf().GetTransactionByBookID(AMID, bookID, startDate, endDate, pageSize, pageNum);
+        [ExcelFunction(Name = "GetPosition", IsMacroType = true)]
+        public static void GetPositionAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum)
+        {
+            ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
+            object[,] getArray= { };
+            Task.Factory.StartNew(() =>Thread.Sleep(0))
+            .ContinueWith(t =>
+                ExcelAsyncUtil.QueueAsMacro(() =>
+                {  
+                    TransactionDataAccess TDA = new TransactionDataAccess();
+                    getArray = TDA.Position(AMID, bookID, startDate, pageSize, pageNum);
+                    // Create a Range of the correct size:
+                    int rows = getArray.GetLength(0);
+                    int columns = getArray.GetLength(1);
+                    ExcelReference target = new ExcelReference(caller.RowFirst, caller.RowFirst + rows - 1, caller.ColumnFirst, caller.ColumnFirst + columns - 1, caller.SheetId);
+                    //Assign the Array to the Range in one shot:
+                    target.SetValue(getArray);
+                }));
+        }
 
-        [ExcelFunction(Name = "GetTransactionByTransactionID", Description = "Get transactions according to transaction ID")]
-        [ExcelCommand(ShortCut = "GTT")]
-        public static object GetTransactionByTransactionIDUdf(
-            [ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(Name = "transaction ID")]string transactionID, [ExcelArgument(Name = "startDate")]string startDate, [ExcelArgument(Name = "endDate")]string endDate, [ExcelArgument(Name = "pageSize")]string pageSize, [ExcelArgument(Name = "pageNum")]string pageNum) =>
-            new TransactionUdf().GetTransactionByTransactionID(AMID, transactionID, startDate, endDate, pageSize, pageNum);
+        [ExcelFunction(Name = "GetTransactionByBookID", IsMacroType = true)]
+        public static void GetTransactionByBookIDAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "endDate")]string endDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum)
+        {
+            ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
+            object[,] getArray = { };
+            Task.Factory.StartNew(() => Thread.Sleep(5000))
+            .ContinueWith(t =>
+                ExcelAsyncUtil.QueueAsMacro(() =>
+                {
+                    TransactionDataAccess TDA = new TransactionDataAccess();
+                    getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, "TransactionByBookID");
+                    // Create a Range of the correct size:
+                    int rows = getArray.GetLength(0);
+                    int columns = getArray.GetLength(1);
+                    ExcelReference target = new ExcelReference(caller.RowFirst, caller.RowFirst + rows - 1, caller.ColumnFirst, caller.ColumnFirst + columns - 1, caller.SheetId);
+                    //Assign the Array to the Range in one shot:
+                    target.SetValue(getArray);
+                }));
+        }
+
+        [ExcelFunction(Name = "GetTransactionByTransactionID", IsMacroType = true)]
+        public static void GetTransactionByTransactionIDAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "endDate")]string endDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum)
+        {
+            ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
+            object[,] getArray = { };
+            Task.Factory.StartNew(() => Thread.Sleep(5000))
+            .ContinueWith(t =>
+                ExcelAsyncUtil.QueueAsMacro(() =>
+                {
+                    TransactionDataAccess TDA = new TransactionDataAccess();
+                    getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, "TransactionByTransactionID");
+                    // Create a Range of the correct size:
+                    int rows = getArray.GetLength(0);
+                    int columns = getArray.GetLength(1);
+                    ExcelReference target = new ExcelReference(caller.RowFirst, caller.RowFirst + rows - 1, caller.ColumnFirst, caller.ColumnFirst + columns - 1, caller.SheetId);
+                    //Assign the Array to the Range in one shot:
+                    target.SetValue(getArray);
+                }));
+        }
 
         public object GetPositions(string AMID, string bookID, string startDate, string pageSize, string pageNum)
         {
