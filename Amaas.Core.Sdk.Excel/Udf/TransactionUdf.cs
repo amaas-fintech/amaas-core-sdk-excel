0ba44ea84f9d136fca51bc43ffe692ddcff32528
JYL123 <li474412210@gmail.com>
2017-08-18 10:36:41 +0800

    Add asset filed search and transaction field search parameters in udf

--- Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
+++ Amaas.Core.Sdk.Excel/Udf/TransactionUdf.cs
@@ -19,8 +19,8 @@ namespace Amaas.Core.Sdk.Excel.Udf
         {
         }     
 
-        [ExcelFunction(Name = "GetPosition", IsMacroType = true, Description = "Get positions according to Book ID")]
-        public static void GetPositionAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum, [ExcelArgument(AllowReference = true, Name = "optional fields")]string fields)
+        [ExcelFunction(Name = "GetPositionByBookID", IsMacroType = true, Description = "Get positions according to Book ID")]
+        public static void GetPositionAsync([ExcelArgument(AllowReference = true, Name = "Asset manager ID")] string AMID, [ExcelArgument(AllowReference = true, Name = "Book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "Start Date of Position")]string startDate, [ExcelArgument(AllowReference = true, Name = "Number of trnasactions to display on a page")]string pageSize, [ExcelArgument(AllowReference = true, Name = "The page.no")]string pageNum, [ExcelArgument(AllowReference = true, Name = "fields search for position")]string fields)
         {
             ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
             object[,] getArray= { };
@@ -47,7 +47,7 @@ namespace Amaas.Core.Sdk.Excel.Udf
         }
 
         [ExcelFunction(Name = "GetTransactionByBookID", IsMacroType = true, Description = "Get transactions according to Book ID")]
-        public static void GetTransactionByBookIDAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "endDate")]string endDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum, [ExcelArgument(AllowReference = true, Name = "optional fields")]string fields)
+        public static void GetTransactionByBookIDAsync([ExcelArgument(AllowReference = true, Name = "Asset manager ID")] string AMID, [ExcelArgument(AllowReference = true, Name = "Book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "Start date of the transaction")]string startDate, [ExcelArgument(AllowReference = true, Name = "End date of the transaction")]string endDate, [ExcelArgument(AllowReference = true, Name = "Number of trnasactions to display on a page")]string pageSize, [ExcelArgument(AllowReference = true, Name = "The page.no")]string pageNum, [ExcelArgument(AllowReference = true, Name = "Field search for the transaction")]string fields, [ExcelArgument(AllowReference = true, Name = "Field search for the asset")]string assetOptionalFields)
         {
             ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
             object[,] getArray = { };
@@ -56,7 +56,7 @@ namespace Amaas.Core.Sdk.Excel.Udf
                 ExcelAsyncUtil.QueueAsMacro(() =>
                 {
                     TransactionDataAccess TDA = new TransactionDataAccess();
-                    getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, fields, "TransactionByBookID");
+                    getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, fields, assetOptionalFields, "TransactionByBookID");
                     // Create a Range of the correct size:
                     int rows = getArray.GetLength(0);
                     int columns = getArray.GetLength(1);
@@ -75,8 +75,38 @@ namespace Amaas.Core.Sdk.Excel.Udf
                 }));
         }
 
+        [ExcelFunction(Name = "GetFilteredTransactionByBookID", IsMacroType = true, Description = "Get filtered transactions according to Book ID")]
+        public static void GetFilteredTransactionByBookIDAsync([ExcelArgument(AllowReference = true, Name = "Asset manager ID")] string AMID, [ExcelArgument(AllowReference = true, Name = "Book ID")]string bookID, [ExcelArgument(AllowReference = true, Name = "Start date of the transaction")]string startDate, [ExcelArgument(AllowReference = true, Name = "End date of the transaction")]string endDate, [ExcelArgument(AllowReference = true, Name = "Number of trnasactions to display on a page")]string pageSize, [ExcelArgument(AllowReference = true, Name = "The page.no")]string pageNum, [ExcelArgument(AllowReference = true, Name = "Field search for the transaction")]string fields, [ExcelArgument(AllowReference = true, Name = "Field search for the asset")]string assetOptionalFields)
+        {
+            ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
+            object[,] getArray = { };
+            Task.Factory.StartNew(() => Thread.Sleep(0)) //Thread.Sleep(5000)
+            .ContinueWith(t =>
+                ExcelAsyncUtil.QueueAsMacro(() =>
+                {
+                    TransactionDataAccess TDA = new TransactionDataAccess();
+                    getArray = TDA.TransactionFiltered (AMID, bookID, startDate, endDate, pageSize, pageNum, fields, assetOptionalFields, "TransactionByBookID");
+                    // Create a Range of the correct size:
+                    int rows = getArray.GetLength(0);
+                    int columns = getArray.GetLength(1);
+                    ExcelReference target = new ExcelReference(caller.RowFirst, caller.RowFirst + rows, caller.ColumnFirst, caller.ColumnFirst + columns - 1, caller.SheetId);
+                    //Assign the Array to the Range in one shot:
+                    if (getArray.GetLength(0) == 1 && getArray.GetLength(1) == 2) target.SetValue("The data does not exist.");
+                    else
+                    {
+                        object[,] resizeArray = new object[rows + 1, columns];
+                        resizeArray[0, 0] = XlCall.Excel(XlCall.xlfGetFormula, target);
+                        for (int i = 1; i < rows + 1; i++)
+                        {
+                            for (int j = 0; j < columns; j++) resizeArray[i, j] = getArray[i - 1, j];
+                        }
+                        target.SetValue(resizeArray);
+                    }
+                }));
+        }
+
         [ExcelFunction(Name = "GetTransactionByTransactionID", IsMacroType = true, Description = "Get transactions according to transaction ID")]
-        public static void GetTransactionByTransactionIDAsync([ExcelArgument(AllowReference = true, Name = "AMID")] string AMID, [ExcelArgument(AllowReference = true, Name = "transaction ID")]string transactionID, [ExcelArgument(AllowReference = true, Name = "startDate")]string startDate, [ExcelArgument(AllowReference = true, Name = "endDate")]string endDate, [ExcelArgument(AllowReference = true, Name = "pageSize")]string pageSize, [ExcelArgument(AllowReference = true, Name = "pageNum")]string pageNum, [ExcelArgument(AllowReference = true, Name = "optional fields")]string fields)
+        public static void GetTransactionByTransactionIDAsync([ExcelArgument(AllowReference = true, Name = "Asset manager ID")] string AMID, [ExcelArgument(AllowReference = true, Name = "Transaction ID")]string transactionID, [ExcelArgument(AllowReference = true, Name = "Start date of the transaction")]string startDate, [ExcelArgument(AllowReference = true, Name = "End date of the transaction")]string endDate, [ExcelArgument(AllowReference = true, Name = "Number of trnasactions to display on a page")]string pageSize, [ExcelArgument(AllowReference = true, Name = "The page.no")]string pageNum, [ExcelArgument(AllowReference = true, Name = "Field search for the transaction")]string fields, [ExcelArgument(AllowReference = true, Name = "Field search for the asset")]string assetOptionalFields)
         {
             ExcelReference caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
             object[,] getArray = { };
@@ -85,7 +115,7 @@ namespace Amaas.Core.Sdk.Excel.Udf
                 ExcelAsyncUtil.QueueAsMacro(() =>
                 {
                     TransactionDataAccess TDA = new TransactionDataAccess();
-                    getArray = TDA.Transaction(AMID, transactionID, startDate, endDate, pageSize, pageNum, fields, "TransactionByTransactionID");
+                    getArray = TDA.Transaction(AMID, transactionID, startDate, endDate, pageSize, pageNum, fields, assetOptionalFields, "TransactionByTransactionID");
                     // Create a Range of the correct size:
                     int rows = getArray.GetLength(0);
                     int columns = getArray.GetLength(1);
@@ -108,17 +138,17 @@ namespace Amaas.Core.Sdk.Excel.Udf
             return ArrayResizer.Resize(getArray, "position");
         }
 
-        public object GetTransactionByTransactionID(string AMID, string transactionID, string startDate, string endDate, string pageSize, string pageNum, string fields)
+        public object GetTransactionByTransactionID(string AMID, string transactionID, string startDate, string endDate, string pageSize, string pageNum, string fields, string assetOptionalFields)
         {
             TransactionDataAccess TDA = new TransactionDataAccess();
-            object[,] getArray = TDA.Transaction(AMID, transactionID, startDate, endDate, pageSize, pageNum, fields, "TransactionByTransactionID");
+            object[,] getArray = TDA.Transaction(AMID, transactionID, startDate, endDate, pageSize, pageNum, fields, assetOptionalFields, "TransactionByTransactionID");
             return ArrayResizer.Resize(getArray, "transactions");
         }
 
-        public object GetTransactionByBookID(string AMID, string bookID, string startDate, string endDate, string pageSize, string pageNum, string fields)
+        public object GetTransactionByBookID(string AMID, string bookID, string startDate, string endDate, string pageSize, string pageNum, string fields, string assetOptionalFields)
         {
             TransactionDataAccess TDA = new TransactionDataAccess();
-            object[,] getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, fields, "TransactionByBookID");
+            object[,] getArray = TDA.Transaction(AMID, bookID, startDate, endDate, pageSize, pageNum, fields, assetOptionalFields, "TransactionByBookID");
             return ArrayResizer.Resize(getArray, "transactions");
         }
        
